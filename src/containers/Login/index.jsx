cb16b1a794bdef924b6dd74851e484be67b2fb07
Allysson Ryan <allyssonr002@gmail.com>
2025-01-08 21:54:54 -0300

    feat: Adiciona página de login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,42 @@
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+} from './styles';
+import Logo from '../../assets/Logo.svg';
+
 export function Login() {
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          <br />
+          Acesse com seu <span> Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Button>Entrar</Button>
+        </Form>
+        <p>
+          Não possui conta? <a> Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
   );
 }
